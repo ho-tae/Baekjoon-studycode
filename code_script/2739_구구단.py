f5cf842546ee9c83ea16daa7a98697010f96e665
Heetae Han <wjdgks497@naver.com>
2022-01-13 17:42:44 +0900

    Create 2739_구구단.py

---
+++ code_script/2739_구구단.py
@@ -0,0 +1,9 @@
+# 백준 코딩 2739번 
+
+<img src="https://user-images.githubusercontent.com/86057816/149294923-1b3081f0-8c1e-4275-b529-e0e356f18d4d.png" />
+
+
+n = int(input())
+
+for i in range(9):
+    print(n, "*", i+1, "=", n*(i+1))
